32668770ad41394f5245f59320768dd6f8737e98
SSDolphinSS <dolphinger@yandex.ru>
2024-07-28 15:58:42 +0300

    add file

---
+++ module_7_2.py
@@ -0,0 +1,26 @@
+import io
+
+
+def custom_write(file_name, strings):
+    strings_positions = {}
+    f = open(file_name, 'a+', encoding='utf-8')
+    for line in strings:
+
+        f.write(line, end='\n')
+    # f.(strings, end='\n')
+    # f.si()
+    # f.tell()
+    # f.close()
+    # return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
