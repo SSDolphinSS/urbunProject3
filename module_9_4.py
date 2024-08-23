6a23bb044ab09db5988a6e23edebf9d32635156c
SSDolphinSS <dolphinger@yandex.ru>
2024-08-23 15:32:35 +0300

    Homework 1.0

---
+++ module_9_4.py
@@ -0,0 +1,33 @@
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for item in data_set:
+                file.write(f"{item}\n")
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+from random import choice
+
+
+class MysticBall:
+    def __init__(self, *word):
+        self.word = word
+
+    def __call__(self):
+        return choice(self.word)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
