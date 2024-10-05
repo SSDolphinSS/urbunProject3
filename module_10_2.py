ee95aec63dfa5520f1b725026182bd48660f39d8
SSDolphinSS <dolphinger@yandex.ru>
2024-10-05 13:12:52 +0300

    Homework 1.0

---
+++ module_10_2.py
@@ -0,0 +1,33 @@
+import threading
+import time
+
+class Knight(threading.Thread):
+    def __init__(self, name, power):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.power = power
+        self.enemies = 100
+
+    def run(self):
+        print(f"{self.name}, на нас напали!")
+        days = 0
+        while self.enemies > 0:
+            days += 1
+            time.sleep(1)
+            self.enemies -= self.power
+            if self.enemies < 0:
+                self.enemies = 0
+            print(f"{self.name} сражается {days} день(дня)..., осталось {self.enemies} воинов.")
+        print(f"{self.name} одержал победу спустя {days} дней(дня)!")
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+
+print("Все битвы закончились!")
